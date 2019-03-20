bcb10664c45967bd680d6e88be541833627b495c
bdcostin <bailey.costin@gmail.com>
2019-03-20 11:59:37 -0400

    Added fields to models & created html pages

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-# from .models import User, User_Post
+# from .models import User, UserPost
 
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 
-# @admin.register(User_Post)
+# @admin.register(UserPost)
 # class User_PostAdmin(admin.ModelAdmin):
 #     list_display = ('title', 'user')
 #     exclude = ('slug',)
--- core/models.py
+++ core/models.py
@@ -2,48 +2,67 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
+from datetime import datetime
 
 # Create your models here.
 
+# ForeignKey is stored as an empty string automatically in the database, so null=True is not needed!
+
 class User(AbstractUser):
-    pass
+    '''Model represents the user's post'''
+    username = models.CharField(max_length=15, unique=True, null=False, blank=False)
+    # Can a model have a relationship to a form?
+    # profile_picture = models.OneToOne(null=True, blank=True)
+    slug = models.SlugField()
+    voted = models.ForeignKey('Vote', null=True, blank=True, on_delete = models.SET_NULL)
+    email = models.CharField(max_length=50, null=False, blank=False)
+    gender_pronouns = models.CharField(max_length=15, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    about = models.TextField(max_length=1000, null=True, blank=True)
 
-# class User_Post(models.Model):
-#     '''Model represents the user's posts'''
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=100, help_text='What is your post called?', null=False)
-#     post_url = models.URLField(null=True, blank=True)
-#     body = models.TextField(max_length=1000, null=True, blank=True)
-#     topic = models.ManyToManyField('Topic', related_name='posts')
-#     slug = models.SlugField()
-#     votes = models.ForeignKey('Vote', on_delete = models.SET_NULL, null=True)
-#     comments = models.ForeignKey('Comment', on_delete = models.SET_NULL, null=True)
 
-#     def set_slug(self):
-#         '''Creates a unique slug for every post'''
-#         if self.slug:
-#             return
-#         base_slug = slugify(self.title)
+class UserPost(models.Model):
+    '''Model represents the user's posts'''
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, help_text='What is your post called?', null=False)
+    source_name = models.URLField(help_text='Please enter the base domain of your link, i.e., "cnn.com" or "www.google.com."',null=True, blank=True)
+    post_url = models.URLField(null=True, blank=True)
+    time_posted = models.DateTimeField(auto_now_add=True, blank=True)
+    body = models.TextField(max_length=1000, null=True, blank=True)
+    topic = models.ManyToManyField('Topic', related_name='posts')
+    slug = models.SlugField()
+    votes = models.ForeignKey('Vote', null=True, blank=True, on_delete = models.SET_NULL)
+    comments = models.ForeignKey('Comment', null=True, blank=True, on_delete = models.SET_NULL)
 
-#         slug = base_slug
-#         n = 0
+    # Can we sort via a ForeginKeyField?
+    class Meta:
+        ordering = ['votes']
 
-#         while User_Post.object.filter(slug=slug).count():
-#             n += 1
-#             slug = base_slug + '-' + str(n)
+    def set_slug(self):
+        '''Creates a unique slug for every post'''
+        if self.slug:
+            return
+        base_slug = slugify(self.title)
+
+        slug = base_slug
+        n = 0
+
+        while User_Post.object.filter(slug=slug).count():
+            n += 1
+            slug = base_slug + '-' + str(n)
         
-#         self.slug = slug
+        self.slug = slug
 
-#     def save(self, *args, **kwargs):
-#         '''Hides slug field in admin'''
-#         self.set_slug()
-#         super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        '''Hides slug field in admin'''
+        self.set_slug()
+        super().save(*args, **kwargs)
 
-# class Vote(models.Model):
-#     pass
+class Vote(models.Model):
+    pass
     
-# class Comment(models.Model):
-#     pass
+class Comment(models.Model):
+    user = models.ManyToManyField(User)
 
-# class Topic(models.Model):
-#     pass
+class Topic(models.Model):
+    pass
